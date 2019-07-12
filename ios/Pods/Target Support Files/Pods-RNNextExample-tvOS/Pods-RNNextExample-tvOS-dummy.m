896ea77a41fe56dbf5d1f4c08f7704da076364f9
Shawn Dempsey <shwanton@gmail.com>
2019-07-11 21:01:55 -0700

    npx react-native-cli init RNNextExample --template typescript --version 0.60.3

---
+++ ios/Pods/Target Support Files/Pods-RNNextExample-tvOS/Pods-RNNextExample-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNNextExample_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_RNNextExample_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-RNNextExampleTests/Pods-RNNextExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNNextExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_RNNextExampleTests
+@end
